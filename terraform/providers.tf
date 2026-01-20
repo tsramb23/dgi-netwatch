8bf3f79d3443fc4c215547c29fdfa911711088b9
TsrAmb23 <latsiory@gmail.com>
2026-01-20 15:41:38 +0100

    new changes on terraform

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,9 +5,20 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.20"
     }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
   }
 }
 
+# Provider Google (pour info du cluster)
+provider "google" {
+  project = "dgi-cosmic-20251210-1542"
+  region  = "us-central1"
+}
+
+# Provider Kubernetes (utilise la config générée par gcloud)
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,19 @@
 # No variables required - Kubernetes provider uses kubeconfig from gcloud
 # Pass these via -var flags in the workflow for documentation purposes only
+variable "gcp_project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "dgi-cosmic-20251210-1542"
+}
+
+variable "gcp_region" {
+  description = "GCP Region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "GKE Cluster Name"
+  type        = string
+  default     = "dgi-netwatch-cluster"
+}
