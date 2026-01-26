2fd3824f059ded42b57bac8098e81cb23db64f6e
TsrAmb23 <latsiory@gmail.com>
2026-01-26 09:55:24 +0100

    Test 8: test modif page web

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,7 @@
-# output "frontend_service_name" {
-#   value = kubernetes_service.frontend.metadata[0].name
-# }
+output "frontend_service_name" {
+  value = kubernetes_service.frontend_loadbalancer.metadata[0].name
+}
+
 
 output "backend_service_name" {
   value = kubernetes_service.backend.metadata[0].name
