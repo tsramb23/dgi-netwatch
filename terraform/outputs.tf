eac8b90679b6c53e5023049e8c2122ddbc849948
TsrAmb23 <latsiory@gmail.com>
2026-01-26 09:09:27 +0100

    Suppression output frontend

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "frontend_service_name" {
-  value = kubernetes_service.frontend.metadata[0].name
-}
+# output "frontend_service_name" {
+#   value = kubernetes_service.frontend.metadata[0].name
+# }
 
 output "backend_service_name" {
   value = kubernetes_service.backend.metadata[0].name
